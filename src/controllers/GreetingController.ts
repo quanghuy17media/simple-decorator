4b4b6987b3a037b9caeafd37696575a0569b1e99
Huy Hq <huyhq@Huys-MacBook-Pro-2.local>
2020-02-24 08:38:13 +0700

    ✨ Feat: Use RabbitMQ

--- src/controllers/GreetingController.ts
+++ src/controllers/GreetingController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { VmoControllerAction, VmoController, RequestMethod } from 'vmo/common';
+import amqp from 'amqplib';
 import { checkHealth, indexData, search } from '../services/test.service';
 import { IIndexSomeDataReq, ISearchReq } from '../types/customTypes';
 
@@ -34,6 +35,25 @@ export class GreetingController {
     // };
     const data = await search(req.body);
     return res.json({ data });
+  }
+
+  @VmoControllerAction({ method: RequestMethod.Post, path: '/send' })
+  async send(req: Request, res: Response) {
+    try {
+      const connect = await amqp.connect({
+        hostname: global.gConfig.amqpUri,
+        username: global.gConfig.amqpUser,
+        password: global.gConfig.amqpPass,
+      });
+      const q = 'vQueues';
+      const ch = await connect.createChannel();
+      await ch.assertQueue(q);
+      ch.sendToQueue(q, Buffer.from('something to do'));
+    } catch (e) {
+      console.log('TCL: GreetingController -> send -> e', e);
+      return res.json(e);
+    }
+    return res.json({ data: 1 });
   }
 
   // @VmoControllerAction({ method: RequestMethod.Get, path: '/test' })
